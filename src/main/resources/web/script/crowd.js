b1df248f08f5b755ef0bf52143cb5cfa920820fd
Jörg Richter <jri@deepamehta.de>
2015-12-18 20:57:12 +0100

    Display bustour GeoJSON.
    See #914.

--- src/main/resources/web/script/crowd.js
+++ src/main/resources/web/script/crowd.js
@@ -1,5 +1,3 @@
-CROWD = {};
-
 angular.module("crowd", [/*"ngRoute"*/ "leaflet-directive"])
 /*.config(function($routeProvider) {
     $routeProvider
@@ -25,32 +23,22 @@ angular.module("crowd", [/*"ngRoute"*/ "leaflet-directive"])
             accessToken: 'pk.eyJ1IjoianJpIiwiYSI6ImNpaG5ubmtsdDAwaHB1bG00aGk1c3BhamcifQ.2XkYFs4hGOel8DYCy4qKKw'
         }
     }
+
+    crowdService.loadBustourGeojson(function(data) {
+        $scope.bustour = {
+            data: data,
+            style: {
+                color: "#f86767"
+            }
+        }
+    })
+
     crowdService.getEvents(function(events) {
         console.log("Events", events)
         events.items.forEach(function(evt) {
             createMarker(evt);
         });
     })
-
-    function createMap() {
-        var map = L.map("map").setView([55, 20], 4);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18,
-            id: 'jri.oaem7303',
-            accessToken: 'pk.eyJ1IjoianJpIiwiYSI6ImNpaG5ubmtsdDAwaHB1bG00aGk1c3BhamcifQ.2XkYFs4hGOel8DYCy4qKKw'
-        }).addTo(map);
-        L.geoJson(CROWD.tourGeoJson, {
-            style: function(feature) {
-                return {
-                    color: feature.properties.stroke
-                }
-            }
-        }).addTo(map)
-        return map;
-    }
 
     function createMarker(evt) {
         var geoCoordinate = evt.childs["dm4.contacts.address"].childs["dm4.geomaps.geo_coordinate"].childs;
@@ -61,7 +49,11 @@ angular.module("crowd", [/*"ngRoute"*/ "leaflet-directive"])
 })    
 .service("crowdService", function($http) {
 
+    this.loadBustourGeojson = function(callback) {
+        $http.get("json/bustour.geo.json").success(callback);
+    }
+
     this.getEvents = function(callback) {
-        $http.get("/core/topic/by_type/dm4.events.event?include_childs=true").success(callback)
+        $http.get("/core/topic/by_type/dm4.events.event?include_childs=true").success(callback);
     }
 })

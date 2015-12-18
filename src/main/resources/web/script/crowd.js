0665eb97cefaa095c92b5637232607403246d3ff
Jörg Richter <jri@deepamehta.de>
2015-12-18 20:22:52 +0100

    Add angular-leaflet-directive.
    See #914.

--- src/main/resources/web/script/crowd.js
+++ src/main/resources/web/script/crowd.js
@@ -1,23 +1,34 @@
 CROWD = {};
 
-angular.module("crowd", ["ngRoute"])
-.config(function($routeProvider) {
+angular.module("crowd", [/*"ngRoute"*/ "leaflet-directive"])
+/*.config(function($routeProvider) {
     $routeProvider
         .when("/event/:eventId", {
             templateUrl: "partials/event.html", controller: "eventController"
         })
-})
-.controller("crowdController", function($scope, crowdService) {
+        .otherwise({redirectTo: "/event"})
+})*/
+.controller("crowdController", function($scope, $location, crowdService) {
 
-    var map = createMap();
+    $scope.center = {
+        lat: 55,
+        lng: 20,
+        zoom: 4
+    }
+    $scope.tiles = {
+        url: "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+        options: {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'jri.oaem7303',
+            accessToken: 'pk.eyJ1IjoianJpIiwiYSI6ImNpaG5ubmtsdDAwaHB1bG00aGk1c3BhamcifQ.2XkYFs4hGOel8DYCy4qKKw'
+        }
+    }
     crowdService.getEvents(function(events) {
         console.log("Events", events)
-        events.items.forEach(function(event) {
-            var geoCoordinate = event.childs["dm4.contacts.address"].childs["dm4.geomaps.geo_coordinate"].childs;
-            L.marker([
-                geoCoordinate["dm4.geomaps.latitude"].value,
-                geoCoordinate["dm4.geomaps.longitude"].value
-            ]).addTo(map);
+        events.items.forEach(function(evt) {
+            createMarker(evt);
         });
     })
 
@@ -39,6 +50,13 @@ angular.module("crowd", ["ngRoute"])
             }
         }).addTo(map)
         return map;
+    }
+
+    function createMarker(evt) {
+        var geoCoordinate = evt.childs["dm4.contacts.address"].childs["dm4.geomaps.geo_coordinate"].childs;
+        // geoCoordinate["dm4.geomaps.latitude"].value
+        // geoCoordinate["dm4.geomaps.longitude"].value
+        // $scope.event = evt;
     }
 })    
 .service("crowdService", function($http) {

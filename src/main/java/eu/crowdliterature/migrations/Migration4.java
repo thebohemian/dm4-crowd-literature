c0ebc1773c4825a8f18721292c6293bf895a7628
Jörg Richter <jri@deepamehta.de>
2015-12-28 20:11:07 +0100

    Model: add Gender to Person.
    See #914.

--- src/main/java/eu/crowdliterature/migrations/Migration4.java
+++ src/main/java/eu/crowdliterature/migrations/Migration4.java
@@ -6,7 +6,7 @@ import de.deepamehta.core.service.Migration;
 
 
 /**
- * Adds "Place of birth", "Nationality" and "Language" to Person.
+ * Adds "Gender", "Place of birth", "Nationality", and "Language" to Person.
  * Runs ALWAYS.
  * <p>
  * Part of CROWD Literature 0.3
@@ -16,6 +16,9 @@ public class Migration4 extends Migration {
     @Override
     public void run() {
         dms.getTopicType("dm4.contacts.person")
+            .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.aggregation_def",
+                "dm4.contacts.person", "crowd.person.gender", "dm4.core.many", "dm4.core.one"),
+                "dm4.datetime.date#dm4.contacts.date_of_birth")
             .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.aggregation_def", "crowd.person.place_of_birth",
                 "dm4.contacts.person", "dm4.contacts.city", "dm4.core.many", "dm4.core.one"),
                 "dm4.contacts.phone_number#dm4.contacts.phone_entry")

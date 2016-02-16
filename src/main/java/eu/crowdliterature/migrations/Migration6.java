26cea2bd1500a2dfd0aaa2c5e86022cc276941ee
Jörg Richter <jri@deepamehta.de>
2016-02-16 14:35:28 +0100

    Adapt to new ModelFactory (#914).
    See #914

--- src/main/java/eu/crowdliterature/migrations/Migration6.java
+++ src/main/java/eu/crowdliterature/migrations/Migration6.java
@@ -17,22 +17,32 @@ public class Migration6 extends Migration {
     @Override
     public void run() {
         dms.getTopicType("dm4.contacts.person")
-            .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.aggregation_def",
-                "dm4.contacts.person", "crowd.person.gender", "dm4.core.many", "dm4.core.one"),
+            .addAssocDefBefore(
+                mf.newAssociationDefinitionModel("dm4.core.aggregation_def",
+                    "dm4.contacts.person", "crowd.person.gender", "dm4.core.many", "dm4.core.one"
+                ),
                 "dm4.datetime.date#dm4.contacts.date_of_birth")
-            .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.aggregation_def", "crowd.person.place_of_birth",
-                "dm4.contacts.person", "dm4.contacts.city", "dm4.core.many", "dm4.core.one"),
+            .addAssocDefBefore(
+                mf.newAssociationDefinitionModel("dm4.core.aggregation_def", "crowd.person.place_of_birth",
+                    "dm4.contacts.person", "dm4.contacts.city", "dm4.core.many", "dm4.core.one"
+                ),
                 "dm4.contacts.phone_number#dm4.contacts.phone_entry")
-            .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.aggregation_def",
-                "dm4.contacts.person", "crowd.person.nationality", "dm4.core.many", "dm4.core.many"),
+            .addAssocDefBefore(
+                mf.newAssociationDefinitionModel("dm4.core.aggregation_def",
+                    "dm4.contacts.person", "crowd.person.nationality", "dm4.core.many", "dm4.core.many"
+                ),
                 "dm4.contacts.phone_number#dm4.contacts.phone_entry")
-            .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.aggregation_def",
-                "dm4.contacts.person", "crowd.language", "dm4.core.many", "dm4.core.many"),
+            .addAssocDefBefore(
+                mf.newAssociationDefinitionModel("dm4.core.aggregation_def",
+                    "dm4.contacts.person", "crowd.language", "dm4.core.many", "dm4.core.many"
+                ),
                 "dm4.contacts.phone_number#dm4.contacts.phone_entry");
         //
         dms.getTopicType("dm4.events.event")
-            .addAssocDefBefore(new AssociationDefinitionModel("dm4.core.composition_def",
-                "dm4.events.event", "crowd.event.entrance_fee", "dm4.core.one", "dm4.core.one"),
+            .addAssocDefBefore(
+                mf.newAssociationDefinitionModel("dm4.core.composition_def",
+                    "dm4.events.event", "crowd.event.entrance_fee", "dm4.core.one", "dm4.core.one"
+                ),
                 "dm4.webbrowser.url");
     }
 }

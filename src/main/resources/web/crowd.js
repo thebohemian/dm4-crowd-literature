3fcbd517ce2ca14b1c9fb6fc4b45917838c8beb7
Jörg Richter <jri@deepamehta.de>
2016-03-17 01:50:09 +0100

    Add spinning page load indicator (#914).
    Not yet perfect.
    Map tile requests (issued by Leaflet) are not yet indicated. Only requests to the `/crowd` endpoint are.
    
    See #914.

--- src/main/resources/web/crowd.js
+++ src/main/resources/web/crowd.js
@@ -1,5 +1,5 @@
 angular.module("crowd", ["ngRoute", "leaflet-directive"])
-.config(function($routeProvider, $logProvider, $httpProvider) {
+.config(function($routeProvider) {
     $routeProvider
         .when("/welcome",             {templateUrl: "views/welcome.html"})
         .when("/person/:personId",    {templateUrl: "views/person.html",       controller: "PersonController"})
@@ -9,6 +9,33 @@ angular.module("crowd", ["ngRoute", "leaflet-directive"])
                                       {templateUrl: "views/event-series.html", controller: "EventSeriesController"})
         .when("/event/:eventId",      {templateUrl: "views/event.html",        controller: "EventController"})
         .otherwise({redirectTo: "/welcome"})
+})
+.config(function($logProvider) {
     $logProvider.debugEnabled(false);
+})
+.config(function($httpProvider) {
+    // console.log("Configuring request/response interceptors")
     $httpProvider.useLegacyPromiseExtensions(false);
+    $httpProvider.interceptors.push(function($injector) {
+        var $http
+        return {
+            request: function(config) {
+                // console.log("Loading starts")
+                setLoadingWidgetDisplay("block")
+                return config
+            },
+            response: function(response) {
+                $http = $http || $injector.get("$http")     // getting $http lazily avoids circular dependency
+                if (!$http.pendingRequests.length) {
+                    // console.log("Loading complete")
+                    setLoadingWidgetDisplay("none")
+                }
+                return response
+            }
+        }
+
+        function setLoadingWidgetDisplay(display) {
+            document.getElementById("spinner").style.display = display
+        }
+    })
 })

1bc197b5858a1f4b17fea2d171119ef3e7b56039
Robert Schuster <thebohemian@gmx.net>
2016-09-08 09:22:15 +0200

    * separated namespace from crowd literature tour and crowd meet

--- src/main/resources/web/services/crowd-service.js
+++ src/main/resources/web/services/crowd-service.js
@@ -1,37 +1,33 @@
 angular.module("crowd").service("crowdService", function($http) {
 
     this.getStartPageContent = function(callback) {
-        $http.get("/crowd/start_page").then(callback);
-    }
-
-    this.loadBustourGeojson = function(callback) {
-        $http.get("crowd-bustour.geo.json").then(callback);
+        $http.get("/crowdmeet/start_page").then(callback);
     }
 
     // --- Person ---
 
     this.getPerson = function(personId, callback) {
-        $http.get("/crowd/person/" + personId).then(callback);
+        $http.get("/crowdmeet/person/" + personId).then(callback);
     }
 
     this.getAllPersons = function(callback) {
-        return $http.get("/crowd/persons").then(callback);
+        return $http.get("/crowdmeet/persons").then(callback);
     }
 
     // --- Work ---
 
     this.getWork = function(workId, callback) {
-        $http.get("/crowd/work/" + workId).then(callback);
+        $http.get("/crowdmeet/work/" + workId).then(callback);
     }
 
     // --- Institution ---
 
     this.getInstitution = function(instId, callback) {
-        $http.get("/crowd/institution/" + instId).then(callback);
+        $http.get("/crowdmeet/institution/" + instId).then(callback);
     }
 
     this.getAllInstitutions = function(callback) {
-        return $http.get("/crowd/institutions").then(callback);
+        return $http.get("/crowdmeet/institutions").then(callback);
     }
 
 })

80b427483194d3da1cdf4bdb091232ef6270f2e3
Robert Schuster <thebohemian@gmx.net>
2017-02-13 21:25:44 +0100

    * improved parent finding algo

--- src/main/java/eu/crowdliterature/DTOHelper.java
+++ src/main/java/eu/crowdliterature/DTOHelper.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.codehaus.jettison.json.JSONException;
 
+import de.deepamehta.core.RelatedTopic;
 import de.deepamehta.core.Topic;
 import eu.crowdliterature.CrowdService.SearchResult;
 
@@ -23,26 +24,28 @@ public class DTOHelper {
 		
 		Set<Long> filter = new HashSet<Long>();
 		for (Topic topic : topics) {
-			// Goes "up" in the type hierarchy to find out whether the topic is
-			// in a hierarchy that interests us.
-			do {
-				String type = topic.getTypeUri();
-				if (acceptableTypes.contains(type)) {
-					filter.add(topic.getId());
-					break;
-				} else {
-					topic = safeParentOrNull(topic);
-				}
-				
-			} while (topic != null);
+			iterate(topics, filter);
 		}
 		json.put("filter", filter);
 		
 		return json;
 	}
 	
-	private static Topic safeParentOrNull(Topic topic) {
-		return (topic != null) ? topic.getRelatedTopic(null, "dm4.core.child", "dm4.core.parent", null) : null;
+	private static void iterate(List<? extends Topic> topics, Set<Long> filter) {
+		for (Topic topic : topics) {
+			// Goes "up" in the type hierarchy to find out whether the topic is
+			// in a hierarchy that interests us.
+			String type = topic.getTypeUri();
+			if (acceptableTypes.contains(type)) {
+				filter.add(topic.getId());
+			} else {
+				iterate(parents(topic), filter);
+			}
+		}
+	}
+	
+	private static List<RelatedTopic> parents(Topic topic) {
+		return topic.getRelatedTopics((String) null, "dm4.core.child", "dm4.core.parent",(String) null);
 	}
 	
 	static class SearchResultImpl extends JSONEnabledImpl implements SearchResult { }

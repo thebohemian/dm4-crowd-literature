98e24b4bd4265f347ea809efe5fe5baaab1a920c
Robert Schuster <thebohemian@gmx.net>
2017-03-05 15:41:54 +0100

    * added default value

--- src/main/resources/web/directives/dm4-event-list.js
+++ src/main/resources/web/directives/dm4-event-list.js
@@ -4,6 +4,9 @@ angular.module("crowd").directive("dm4EventList", function() {
         scope: {
             events: "="
         },
+        link: function (scope, element, attrs) {
+          scope.title = attrs['title'] || "Events";
+        },
         templateUrl: "views/event-list.html"
     }
 })
